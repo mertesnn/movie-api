204eb2193240da92aeef27a9ff4d6d003617237d
mertesnn <mert_esen_@hotmail.com>
2021-11-11 17:11:43 +0300

    Database name changed and movies endpoint

--- helper/config.js
+++ helper/config.js
@@ -1,11 +1,13 @@
 const mongoose = require( 'mongoose' );
 
 module.exports = () => {
-    mongoose.connect( 'mongodb+srv://tilky:ghvyftGrsTy0ayLt@movieapp.g0i93.mongodb.net/test' );
+    mongoose.connect( 'mongodb+srv://tilky:ghvyftGrsTy0ayLt@movieapp.g0i93.mongodb.net/movie-app' );
     mongoose.connection.on( 'open', () => {
         console.log( 'MongoDB: Connected.' );
     });
     mongoose.connection.on( 'error', (err) => {
         console.log( 'MongoDB: Error - ', err );
     });
+
+    mongoose.Promise = global.Promise;
 };
--- routes/movie.js
+++ routes/movie.js
@@ -1,8 +1,21 @@
-var express = require( 'express' );
-var router  = express.Router();
+const express = require( 'express' );
+const router  = express.Router();
 
-router.get( '/', ( req , res , next ) => {
-  res.send( 'respond with a resource' );
+// Models
+const Movie   = require( '../models/Movie' );
+
+router.post( '/', ( req , res , next ) => {
+  // const { title , imdb_score , category , country , year } = req.body;
+
+  const movie = new Movie( req.body );
+
+  movie.save()
+      .then( ( data )  => {
+    res.json( { status : 1 } );
+      })
+      .catch( ( err ) => {
+    res.json( err );
+      });
 });
 
 module.exports = router;
