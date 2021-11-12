076676321a76823e5a1c4b45e3a2071cdd36fa1e
mertesnn <mert_esen_@hotmail.com>
2021-11-12 16:44:52 +0300

    Movie model validation.

--- models/Movie.js
+++ models/Movie.js
@@ -2,18 +2,35 @@ const mongoose  = require( 'mongoose' );
 const Schema    = mongoose.Schema;
 
 const MovieSchema = new Schema({
-    director_id : Schema.Types.ObjectId,
-    title       : {
-        type    : String,
-        required: true
+    director_id     : Schema.Types.ObjectId,
+    title           : {
+        type        : String,
+        required    : [ true , '`{PATH}` field is required.' ],
+        maxlength   : [ 20 , '`{PATH}` field must be less than `{MAXLENGTH}` characters.' ],
+        minlength   : [ 3 , '`{PATH}` field must be more than `{MINLENGTH}` characters.' ]
     },
-    category    : String,
-    country     : String,
-    year        : Number,
-    imdb_score  : Number,
-    createdAt   : {
-        type    : Date,
-        default : Date.now
+    category        : {
+        type        : String,
+        required    : true,
+        maxlength   : 30,
+        minlength   : 5
+    },
+    country         : {
+        type        : String,
+        required    : true,
+        maxlength   : 50,
+        minlength   : 3
+    },
+    year            : {
+        type        : Number,
+        required    : true,
+        max         : 2050,
+        min         : 1900
+    },
+    imdb_score      : Number,
+    createdAt       : {
+        type        : Date,
+        default     : Date.now
     }
 });
 
