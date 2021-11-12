9b9c6d461e42358ba264dd8bbafb3033e80bfc18
mertesnn <mert_esen_@hotmail.com>
2021-11-12 12:13:25 +0300

    Update, Delete and Between endpoint added.

--- routes/movie.js
+++ routes/movie.js
@@ -1,9 +1,10 @@
 const express = require( 'express' );
 const router  = express.Router();
 
-// Models
+// Model
 const Movie   = require( '../models/Movie' );
 
+// `POST` => Create a new movie.
 router.post( '/', ( req , res , next ) => {
   // const { title , imdb_score , category , country , year } = req.body;
 
@@ -11,13 +12,14 @@ router.post( '/', ( req , res , next ) => {
 
   movie.save()
       .then( ( data )  => {
-        res.json( { status : 1 } );
+        res.json(  data );
       })
       .catch( ( err ) => {
         res.json( err );
       });
 });
 
+// `GET` => List all movies.
 router.get( '/' , ( req , res ) => {
     Movie.find( {  } )
         .then( ( data ) => {
@@ -28,8 +30,67 @@ router.get( '/' , ( req , res ) => {
         });
 });
 
+// `GET` => Get the Top10 movies.
+router.get( '/top10' , ( req , res , next ) => {
+    Movie.find( {  } )
+        .sort( { imdb_score : -1 } )
+        .limit( 10 )
+        .then( ( data ) => {
+            if ( !data )
+                return next( { errorCode : 1, errorMessage : 'Movie not found.' } );
+
+            res.json( data );
+        })
+        .catch( ( err ) => {
+            res.json( err );
+        });
+});
+
+// `GET` => Get a movie by ID.
 router.get( '/:movie_id' , ( req , res , next ) => {
     Movie.findById( req.params.movie_id )
+        .then( ( data ) => {
+            if ( !data )
+                return next( { errorCode : 1, errorMessage : 'Movie not found.' } );
+
+            res.json( data );
+        })
+        .catch( ( err ) => {
+            res.json( err );
+        });
+});
+
+// `PUT` => Update a movie by ID.
+router.put( '/:movie_id' , ( req , res , next) =>  {
+    Movie.findByIdAndUpdate( req.params.movie_id , req.body , { new : true } )
+        .then( ( data ) => {
+            if ( !data )
+                return next( { errorCode : 1, errorMessage : 'Movie not found.' } );
+
+            res.json( data );
+        })
+        .catch( ( err ) => {
+            res.json( err );
+        });
+});
+
+// `DELETE` => Delete a movie by ID.
+router.delete( '/:movie_id' , ( req, res , next ) => {
+   Movie.findByIdAndDelete( req.params.movie_id )
+       .then( ( data ) => {
+           if ( !data )
+               return next( { errorCode : 1, errorMessage : 'Movie not found.' } );
+
+           res.json( { status : 1 , message : 'Deleted successfully.' } );
+       })
+       .catch( ( err ) => {
+           res.json( err );
+       });
+});
+
+// `GET` => Movies between two dates.
+router.get( '/between/:start_year/:end_year' , ( req , res , next ) => {
+    Movie.find( { year : { $gte : req.params.start_year , $lte : req.params.end_year } } )
         .then( ( data ) => {
             if ( !data )
                 return next( { errorCode : 1, errorMessage : 'Movie not found.' } );
