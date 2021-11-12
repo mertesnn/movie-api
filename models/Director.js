6142c50eb68650a91da4194de7b680825fe7f5e4
mertesnn <mert_esen_@hotmail.com>
2021-11-12 17:05:12 +0300

    Director Schema validation.

--- models/Director.js
+++ models/Director.js
@@ -2,12 +2,27 @@ const mongoose  = require( 'mongoose' );
 const Schema    = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-    name        : String,
-    surname     : String,
-    bio         : String,
-    createdAt   : {
-        type    : Date,
-        default : Date.now
+    name            : {
+        type        : String,
+        required    : true,
+        maxlength   : 50,
+        minlength   : 2
+    },
+    surname         : {
+        type        : String,
+        required    : true,
+        maxlength   : 50,
+        minlength   : 2
+    },
+    bio             : {
+        type        : String,
+        required    : true,
+        maxlength   : 150,
+        minlength   : 2
+    },
+    createdAt       : {
+        type        : Date,
+        default     : Date.now
     }
 });
 
