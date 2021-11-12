a0518a47dbffb0c84bbc75f3b4b7d7cb2267e90c
mertesnn <mert_esen_@hotmail.com>
2021-11-12 17:42:18 +0300

    User model added. bcrypt module added. Register endpoint added.

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose  = require( 'mongoose' );
+const Schema    = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username            : {
+        type            : String,
+        required        : true,
+        unique          : true,
+        maxlength       : 25,
+        minlength       : 6
+    },
+    password            : {
+        type            : String,
+        maxlength       : 50,
+        minlength       : 5
+    }
+});
+
+module.exports = mongoose.model( 'user' , UserSchema );
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const express = require( 'express' );
 const router  = express.Router();
 
+// Model
+const User    = require( '../models/User' );
+
 router.get( '/', ( req , res , next ) => {
   res.render( 'index', { title : 'Express' } );
+});
+
+router.post( '/register' , ( req , res , next) => {
+  const user = new User( req.body );
+
+  user.save()
+      .then( ( data ) => {
+        res.json( data );
+      })
+      .catch( ( err ) => {
+        res.json( err );
+      });
 });
 
 module.exports = router;
