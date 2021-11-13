1278e1dfeee666fab8fa4f624bdf6a2b100854f0
mertesnn <mert_esen_@hotmail.com>
2021-11-13 17:34:10 +0300

    Index page test added.

---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+const chai      = require( 'chai' );
+const chaiHttp  = require( 'chai-http' );
+const server    = require( '../app' );
+const should    = chai.should();
+
+chai.use( chaiHttp );
+
+describe( 'Node Server' , () => {
+    it( '( GET / ) Index Page' , ( done ) => {
+        chai.request( server )
+            .get( '/' )
+            .end( ( err , res ) => {
+                res.should.have.status( 200 );
+                done();
+            })
+    });
+});
